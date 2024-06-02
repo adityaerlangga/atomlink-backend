0e9eeded6f97c4da043a01ee369e1e544ea23252
Aditya Erlangga Wibowo <65293996+adityaerlangga@users.noreply.github.com>
2024-06-03 01:25:32 +0700

    add clear all cache route

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,51 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\Api\OwnerController;
 use App\Http\Controllers\Api\TopupController;
 use App\Http\Controllers\Api\OutletController;
 use App\Http\Controllers\Api\VariableController;
 use App\Http\Controllers\Api\WorkshopController;
 
+// ======================= START: SETUP ======================= //
+Route::get('setup', function() {
+    Artisan::call('migrate:fresh --seed');
+    Artisan::call('optimize:clear');
+    Artisan::call('storage:link');
+
+    return response()->json([
+        'message' => 'Setup completed'
+    ]);
+});
+
+// Optimize and clear all cache
+Route::get('optimize', function() {
+    Artisan::call('optimize:clear');
+    Artisan::call('route:clear');
+    Artisan::call('config:clear');
+    Artisan::call('view:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('event:clear');
+
+
+    return response()->json([
+        'message' => 'Optimize and clear cache completed'
+    ]);
+});
+
+
+// CRONJOB AUTO CANCEL EXPIRED TOPUP //
+Route::get('auto_cancel', [TopupController::class, 'autoCancel']);
+
+// ======================= END: SETUP ======================= //
+
 Route::post('requestOtp', [OwnerController::class, 'requestOtp']);
 Route::post('validateOtp', [OwnerController::class, 'validateOtp']);
 Route::post('owner/register', [OwnerController::class, 'register']);
 Route::post('owner/login', [OwnerController::class, 'login']);
 
-// CRONJOB AUTO CANCEL EXPIRED TOPUP //
-Route::get('auto_cancel', [TopupController::class, 'autoCancel']);
+
 
 Route::prefix('variable')->group(function() {
     Route::get('cities', [VariableController::class, 'cities']);
