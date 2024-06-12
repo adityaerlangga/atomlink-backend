85d7ea4c6ced434cd6fa7b02e8e05ea2e6784956
Aditya Erlangga Wibowo <65293996+adityaerlangga@users.noreply.github.com>
2024-06-12 10:42:50 +0700

    add comment for testing changes

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -18,5 +18,5 @@ class DashboardController extends ApiController
     }
 
     // CHANGES TEST FOR PULLING GIT
-    // COUNTS: 4
+    // COUNTS: 5
 }
--- pull.sh
+++ pull.sh
@@ -1,1 +1,2 @@
+cd /home/tamq2554/public_html/atomlink.my.id/
 git pull origin develop
