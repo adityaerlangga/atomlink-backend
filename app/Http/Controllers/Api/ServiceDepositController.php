a3b65176f843a100f136147d0370b21fecc37dad
Aditya Erlangga Wibowo <65293996+adityaerlangga@users.noreply.github.com>
2024-06-14 10:20:15 +0700

    fix service-deposits

--- app/Http/Controllers/Api/ServiceDepositController.php
+++ app/Http/Controllers/Api/ServiceDepositController.php
@@ -182,38 +182,21 @@ class ServiceDepositController extends ApiController
     public function update(Request $request, $service_deposit_code)
     {
         $rules = [
-            'outlet_code' => 'required|string|max:255',
             'service_code' => 'required|string|max:255',
             'service_deposit_name' => 'required|string|max:255',
             'service_deposit_quota' => 'required|numeric',
             'service_deposit_discount_percentage' => 'required|numeric|max:100',
             'service_deposit_price' => 'required|numeric',
             'service_deposit_period_type' => 'required|string|in:UNLIMITED,ACTIVE_PERIOD',
-            'service_deposit_active_period_days' => 'required_if:service_deposit_period_type,1|numeric',
-            'service_deposit_active_period_type' => 'required_if:service_deposit_period_type,1|string|in:ACCUMULATION,OLDEST,NEWEST',
-            'service_deposit_expired_action' => 'required_if:service_deposit_period_type,1|string|in:BURN,ROLL_UP',
+            'service_deposit_active_period_days' => 'nullable|required_if:service_deposit_period_type,ACTIVE_PERIOD|numeric',
+            'service_deposit_active_period_type' => 'nullable|required_if:service_deposit_period_type,ACTIVE_PERIOD|string|in:ACCUMULATION,OLDEST,NEWEST',
+            'service_deposit_expired_action' => 'nullable|required_if:service_deposit_period_type,ACTIVE_PERIOD|string|in:BURN,ROLL_UP',
         ];
 
         $validator = validateThis($request, $rules);
 
         if ($validator->fails()) {
             return $this->sendError(2, 'Params not complete', validationMessage($validator->errors()));
-        }
-
-        $outlet = Outlet::where('outlet_code', $request->outlet_code)
-            ->where('is_deleted', 0)
-            ->first();
-
-        if (!$outlet) {
-            return $this->sendError(1, "Outlet tidak ditemukan", null);
-        }
-
-        $service = Service::where('service_code', $request->service_code)
-            ->where('is_deleted', 0)
-            ->first();
-
-        if (!$service) {
-            return $this->sendError(1, "Layanan Regular tidak ditemukan", null);
         }
 
         $service_deposit = ServiceDeposit::where('service_deposit_code', $service_deposit_code)
@@ -224,8 +207,16 @@ class ServiceDepositController extends ApiController
             return $this->sendError(1, "Deposit Layanan tidak ditemukan", null);
         }
 
+        $service = Service::where('service_code', $request->service_code)
+            ->where('is_deleted', 0)
+            ->first();
+
+        if (!$service) {
+            return $this->sendError(1, "Layanan Regular tidak ditemukan", null);
+        }
+
         DB::beginTransaction();
-        try {
+        try{
             $service_deposit_price = $request->service_deposit_price;
 
             $actual_deposit_price_total = $service->service_price * $request->service_deposit_quota;
@@ -242,7 +233,6 @@ class ServiceDepositController extends ApiController
                 ]);
             }
 
-            $service_deposit->outlet_code = $request->outlet_code;
             $service_deposit->service_code = $request->service_code;
             $service_deposit->service_deposit_name = $request->service_deposit_name;
             $service_deposit->service_deposit_quota = $request->service_deposit_quota;
@@ -255,16 +245,17 @@ class ServiceDepositController extends ApiController
             $service_deposit->update();
 
             if (!$service_deposit) {
-                return $this->sendError(1, "Deposit Layanan gagal diubah", null);
+                return $this->sendError(1, "Deposit Layanan gagal diupdate", null);
             }
 
             $service_deposit->service = $service;
 
             DB::commit();
-            return $this->sendResponse(0, "Deposit Layanan berhasil diubah", $service_deposit);
+            return $this->sendResponse(0, "Deposit Layanan berhasil diupdate", $service_deposit);
+
         } catch (\Exception $e) {
             DB::rollBack();
-            return $this->sendError(2, "Deposit Layanan gagal diubah", $e->getMessage());
+            return $this->sendError(2, "Deposit Layanan gagal diupdate", $e->getMessage());
         }
     }
 
